238193aef631e40233ba1a1da677544b8db83e74
Isaac Lewis <ikelewis678@gmail.com>
2011-11-29 20:38:04 -0800

    Remove deprecated code.

--- futurefly.js
+++ futurefly.js
@@ -1,11 +1,1 @@
-var imgs = document.getElementsByTagName('img');
-
-function getSources() {
-	for(var i = 0; i < imgs.length; i++)
-	{
-	   if(imgs[i].nextSibling.tagName == "source")
-	   {
-	     alert('found a source tag after an image');
-	   }
-	}
-}
+// This will be loaded inline, and determine what src to use for each <picture>'s img tag.
